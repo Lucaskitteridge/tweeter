add67a22a4a940531b3cf5b3fdfa56934e7cb592
Lucas Kitteridge <lucas@MacBook.local>
2020-12-16 21:16:01 -0500

    added error display and linted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,34 +1,10 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement = function(tweetObject) {
+  
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
     
   const $tweet = $(`<section class="tweet-container">
         <header class="header">
@@ -41,32 +17,68 @@ const createTweetElement = function(tweetObject) {
           </span>
         </header>
         <p class="actualtweet">
-            ${tweetObject.content.text}
+            ${escape(tweetObject.content.text)}
         </p>
         <span class="borderline"></span>
         <footer class="feedfooter">
-          <span class="dateofpost">${tweetObject.created_at}</span>
+          <span class="dateofpost">${new Date(tweetObject.created_at)}</span>
           <div class="reposts">
             <i class="fas fa-flag blue"></i>
             <i class="fas fa-retweet blue"></i>
             <i class="fas fa-heart blue" ></i>
           </div>
         </footer>
-      </section>`)
+      </section>`);
 
-  return $tweet
-}
+  return $tweet;
+};
 
 const renderTweets = function(tweets) {
-  // loops through tweets
-  for(let indtweet of tweets) {
-    $('.tweets-container').prepend(createTweetElement(indtweet))
+  for (let indtweet of tweets) {
+    $('.tweets-container').prepend(createTweetElement(indtweet));
   }
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-}
+};
 
 $(document).ready(function() {
-  renderTweets(data)
-})
 
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:8080/tweets'
+    })
+      .then((result) => {
+        renderTweets(result);
+      });
+  };
+
+  loadTweets();
+
+  $("#input-form").on('submit', function(event) {
+
+    event.preventDefault();
+
+    const inputbox = $(this).serialize();
+
+    if ($('#tweet-text').val().length > 140) {
+      $('.error-message').css("display", "block");
+      $('.error-message').text("Too much tweeting, bring it under 140 characters");
+
+      return;
+    }
+    if ($('#tweet-text').val().length === 0) {
+      $('.error-message').css("display", "block");
+      $('.error-message').text("Nothing to tweet about? Please tweet about something");
+      return;
+    }
+    $('.error-message').css("display", "none");
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:8080/tweets',
+      data: inputbox,
+      success: function() {
+        loadTweets();
+        $('#input-form')[0].reset();
+      }
+    });
+  });
+});
