9cc754fe6e404b6f4a8f8bc84be3f052eabc62aa
Lucas Kitteridge <lucas@MacBook.local>
2020-12-16 21:16:23 -0500

    linted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $("textarea").keyup(function() {
-    const $counter = $(this).parent().siblings(".lastline").children(".counter")
-    $counter.text(140 - $(this).val().length)
-    if($counter.text() < 0) {
-      $counter.css("color", "red")
-    } else($counter.css("color", "black"))
-  })
+    const $counter = $(this).parent().siblings(".lastline").children(".counter");
+    $counter.text(140 - $(this).val().length);
+    if ($counter.text() < 0) {
+      $counter.css("color", "red");
+    } else ($counter.css("color", "black"));
+  });
 });
 
